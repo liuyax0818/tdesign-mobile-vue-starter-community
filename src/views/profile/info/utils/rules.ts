4a572d8ab93c8bc1c3d7df24c22c81be823fcb24
Reuse5653 <tcoinsm@163.com>
2025-08-12 03:09:20 +0800

    feat(info): 更新个人信息表单验证规则

--- src/views/profile/info/utils/rules.ts
+++ src/views/profile/info/utils/rules.ts
@@ -1,10 +1,47 @@
-import type { FormRules } from 'tdesign-mobile-vue'
+import type { FormRules, UploadFile } from 'tdesign-mobile-vue'
 import type { ProfileForm } from './types'
 
 export const rules: FormRules<ProfileForm> = {
-  username: [{ validator: (val: any) => val !== '', message: '请输入用户名', trigger: 'blur' }],
-  gender: [{ validator: (val: any) => val !== '', message: '请选择性别', trigger: 'change' }],
-  birthday: [{ validator: (val: any) => val !== '', message: '请选择性别', trigger: 'change' }],
-  address: [{ validator: (val: any) => val !== '', message: '请选择地址', trigger: 'change' }],
-  bio: [{ validator: (val: any) => val !== '', message: '请输入个人简介', trigger: 'blur' }],
+  username: [
+    { required: true, message: '用户名不能为空', trigger: 'blur' },
+    { min: 2, message: '用户名至少需要2个字符', trigger: 'blur' },
+    { max: 20, message: '用户名不能超过20个字符', trigger: 'blur' },
+    {
+      pattern: /^[\u4E00-\u9FA5\w]+$/,
+      message: '用户名只能包含中文、英文、数字和下划线',
+      trigger: 'blur',
+    },
+  ],
+  gender: [
+    { required: true, message: '请选择性别', trigger: 'change' },
+  ],
+  birthday: [
+    { required: true, message: '请选择生日', trigger: 'change' },
+    {
+      validator: (val: string) => {
+        const birthDate = new Date(val)
+        const today = new Date()
+        if (birthDate.getFullYear() > today.getFullYear()) {
+          return false
+        }
+        const age = today.getFullYear() - birthDate.getFullYear()
+        return age >= 0 && age <= 150
+      },
+      message: '请选择一个有效的生日日期',
+      trigger: 'change',
+    },
+  ],
+  address: [
+    { required: true, message: '请选择地址', trigger: 'change' },
+  ],
+  bio: [
+    { required: true, message: '请输入个人简介', trigger: 'blur' },
+  ],
+  photos: [
+    {
+      validator: (val: UploadFile[]) => val.length <= 12,
+      message: '最多上传12张照片',
+      trigger: 'change',
+    },
+  ],
 }
