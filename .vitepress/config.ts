90fe06ef0e3b84a5f39f03072300af192e0fa3e8
hikari <1803703552@qq.com>
2025-08-13 23:26:01 +0800

    perf(doc): 文档国际化适配

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,25 +4,60 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   srcDir: 'docs',
 
-  title: 'TDesign Docs',
-  description: 'TDesign Mobile Vue Quick Start',
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' },
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' },
+  locales: {
+    root: {
+      title: 'TDesign Docs',
+      description: 'TDesign Mobile Vue Quick Start',
+      label: 'English',
+      lang: 'en',
+      link: '/en',
+      themeConfig: {
+        nav: [
+          { text: 'Home', link: '/en/index' },
+          { text: 'Examples', link: '/en/markdown-examples' },
+        ],
+        sidebar: [
+          {
+            text: 'Examples',
+            items: [
+              { text: 'Markdown Examples', link: '/en/markdown-examples' },
+              { text: 'Runtime API Examples', link: '/en/api-examples' },
+            ],
+          },
         ],
       },
-    ],
+    },
 
+    zh: {
+      title: 'TDesign 文档',
+      description: 'TDesign 移动端模板',
+      label: '中文（简体）',
+      lang: 'zh',
+      link: '/zh',
+      themeConfig: {
+        nav: [
+          { text: '首页', link: '/zh/index' },
+          { text: '示例', link: '/zh/markdown-examples' },
+        ],
+        sidebar: [
+          {
+            text: '示例',
+            items: [
+              { text: 'Markdown 示例', link: '/zh/markdown-examples' },
+              { text: '运行时 API 示例', link: '/zh/api-examples' },
+            ],
+          },
+        ],
+      },
+    },
+  },
+
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+
+    search: {
+      provider: 'local',
+    },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/TDesignOteam/tdesign-mobile-vue-starter-community' },
     ],
