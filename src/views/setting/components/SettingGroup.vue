1a64a92898e03077e5e51eeee5d7c8bcaf9eb389
FunEnn <3095852337@qq.com>
2025-08-28 00:27:45 +0800

    refactor(#2): 设置页groups进行封装

---
+++ src/views/setting/components/SettingGroup.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: 'SettingGroup',
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="setting-group">
+    <t-cell
+      v-for="(item, index) in items"
+      :key="index"
+      :title="item.title"
+      arrow
+      hover
+      @click="item.onClick && item.onClick()"
+    >
+      <template #left-icon>
+        <TIcon :name="item.icon" size="20px" :color="item.color || '#0052d9'" />
+      </template>
+    </t-cell>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.setting-group {
+  background: #fff;
+  border-radius: 12px;
+  margin-bottom: 16px;
+  overflow: hidden;
+}
+
+:deep(.t-cell__left) {
+  display: flex;
+  align-items: center;
+}
+</style>
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,7 +1,28 @@
 <script setup lang='ts'>
+import SettingGroup from './components/SettingGroup.vue'
+
 defineOptions({
   name: 'Setting',
 })
+
+function noop() {}
+
+const groups = [
+  [
+    { title: '通用设置', icon: 'app', onClick: noop },
+    { title: '通知设置', icon: 'notification', onClick: noop },
+    { title: '切换语言', icon: 'internet', onClick: noop },
+  ],
+  [
+    { title: '深色模式', icon: 'image', onClick: noop },
+    { title: '字体大小', icon: 'chart', onClick: noop },
+    { title: '播放设置', icon: 'sound', onClick: noop },
+  ],
+  [
+    { title: '账号安全', icon: 'secured', onClick: noop },
+    { title: '隐私', icon: 'info-circle', onClick: noop },
+  ],
+]
 </script>
 
 <route lang="json5">
@@ -16,69 +37,6 @@ defineOptions({
   <div class="setting-page h-full overflow-y-auto p-[16px]">
     <Banner title="设置" func="back" />
 
-    <div class="setting-group">
-      <t-cell title="通用设置" arrow hover @click="() => {}">
-        <template #left-icon>
-          <TIcon name="app" size="20px" color="#0052d9" />
-        </template>
-      </t-cell>
-      <t-cell title="通知设置" arrow hover @click="() => {}">
-        <template #left-icon>
-          <TIcon name="notification" size="20px" color="#0052d9" />
-        </template>
-      </t-cell>
-
-      <t-cell title="切换语言" arrow hover @click="() => {}">
-        <template #left-icon>
-          <TIcon name="internet" size="20px" color="#0052d9" />
-        </template>
-      </t-cell>
-    </div>
-
-    <div class="setting-group">
-      <t-cell title="深色模式" arrow hover @click="() => {}">
-        <template #left-icon>
-          <TIcon name="image" size="20px" color="#0052d9" />
-        </template>
-      </t-cell>
-      <t-cell title="字体大小" arrow hover @click="() => {}">
-        <template #left-icon>
-          <TIcon name="chart" size="20px" color="#0052d9" />
-        </template>
-      </t-cell>
-      <t-cell title="播放设置" arrow hover @click="() => {}">
-        <template #left-icon>
-          <TIcon name="sound" size="20px" color="#0052d9" />
-        </template>
-      </t-cell>
-    </div>
-
-    <div class="setting-group">
-      <t-cell title="账号安全" arrow hover @click="() => {}">
-        <template #left-icon>
-          <TIcon name="secured" size="20px" color="#0052d9" />
-        </template>
-      </t-cell>
-      <t-cell title="隐私" arrow hover @click="() => {}">
-        <template #left-icon>
-          <TIcon name="info-circle" size="20px" color="#0052d9" />
-        </template>
-      </t-cell>
-    </div>
+    <SettingGroup v-for="(items, idx) in groups" :key="idx" :items="items" />
   </div>
 </template>
-
-<style lang="scss" scoped>
-.setting-group {
-  background: #fff;
-  border-radius: 12px;
-  margin-bottom: 16px;
-  overflow: hidden;
-}
-
-:deep(.t-cell__left) {
-  // 修复图标和标题不对齐的bug
-  display: flex;
-  align-items: center;
-}
-</style>
